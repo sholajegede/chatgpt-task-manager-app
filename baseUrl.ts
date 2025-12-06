11825cb5e5bf58377528667230d0a37bc2163fe9
sholajegede <jegedeshola@gmail.com>
2025-12-06 14:52:13 +0100

    chore: update all files and UI with shadcn components

--- baseUrl.ts
+++ baseUrl.ts
@@ -1,6 +1,6 @@
 export const baseURL =
   process.env.NODE_ENV == "development"
-    ? "http://localhost:8080"
+    ? "http://localhost:3000"
     : "https://" +
       (process.env.VERCEL_ENV === "production"
         ? process.env.VERCEL_PROJECT_PRODUCTION_URL
